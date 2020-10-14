693b38add78e437069246858d4079ae04eca0daa
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-14 19:04:20 +0530

    Added method to Login

--- QuotesCrawler/spiders/quotes_crawler.py
+++ QuotesCrawler/spiders/quotes_crawler.py
@@ -1,14 +1,25 @@
 import scrapy
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 from ..items import QuotescrawlerItem
 
 class QuotesCrawler(scrapy.Spider):
     name = "Quotes"
     start_urls=[
-        "http://quotes.toscrape.com/"
+        "http://quotes.toscrape.com/login"
     ]
 
     def parse(self, response):
+        token = response.css('form input::attr(value)').extract_first()
+        return FormRequest.from_response(response, formdata={
+            'csrf_token' : token,
+            'username' : 'hello@gmail.com',
+            'password' : 'password123'
+        }, callback=self.start_scarping)
 
+
+    def start_scarping(self,response):    
+        open_in_browser(response)
         items = QuotescrawlerItem()
 
         all_quotes = response.css('div.quote') 
